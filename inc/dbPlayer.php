58764f1da39ada726e187ed7aec8fae7e051f1a4
Harmony Lumumba Muyagane <lumumbaharmony@gmail.com>
2024-02-27 18:19:11 +0300

    Update dbPlayer.php

--- inc/dbPlayer.php
+++ inc/dbPlayer.php
@@ -8,211 +8,132 @@
 
 namespace dbPlayer;
 
-
 class dbPlayer {
 
-    private $db_host="localhost";
-    private $db_name="hms";
-    private $db_user="root";
-    private $db_pass="";
+    private $db_host = "localhost";
+    private $db_name = "hms";
+    private $db_user = "root";
+    private $db_pass = "";
     protected $con;
 
-   public function open(){
-        $con = mysql_connect($this->db_host,$this->db_user,$this->db_pass);
-       if($con)
-       {
-           $dbSelect = mysql_select_db($this->db_name);
-
-           if($dbSelect)
-           {
-               return "true";
-           }
-           else
-           {
-               return mysql_error();
-           }
-
-       }
-        else
-        {
-            return  mysql_error();
-        }
-
-    }
-    public  function close()
-    {
-        $res=mysql_close($this->con);
-        if($res)
-        {
+    public function open() {
+        $this->con = new \mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
+        if ($this->con->connect_errno) {
+            return "Connection failed: " . $this->con->connect_error;
+        } else {
             return "true";
         }
-        else
-        {
-            return mysql_error();
-        }
-
     }
 
-    public function insertData($table,$data)
-    {
-        $keys   = "`" . implode("`, `", array_keys($data)) . "`";
+    public function close() {
+        $this->con->close();
+        return "true";
+    }
+
+    public function insertData($table, $data) {
+        $keys = "`" . implode("`, `", array_keys($data)) . "`";
         $values = "'" . implode("', '", $data) . "'";
-       //var_dump("INSERT INTO `{$table}` ({$keys}) VALUES ({$values})");
-        mysql_query("INSERT INTO `{$table}` ({$keys}) VALUES ({$values})");
-
-        return mysql_insert_id().mysql_error();
-
+        $query = "INSERT INTO `$table` ($keys) VALUES ($values)";
+        if ($this->con->query($query)) {
+            return $this->con->insert_id;
+        } else {
+            return $this->con->error;
+        }
     }
-    public function registration($query,$query2)
-    {
-        $res=mysql_query($query);
-        if($res)
-        {
 
-            $res=mysql_query($query2);
-            if($res)
-            {
-
+    public function registration($query, $query2) {
+        if ($this->con->query($query)) {
+            if ($this->con->query($query2)) {
                 return "true";
+            } else {
+                return $this->con->error;
             }
-            else
-            {
-                return mysql_error();
-            }
-
+        } else {
+            return $this->con->error;
         }
-        else
-        {
-            return mysql_error();
-        }
-
-
     }
-    public  function  getData($query)
-    {
-        $res = mysql_query($query);
-        if(!$res)
-        {
-            return "Can't get data ".mysql_error();
-        }
-        else
-        {
-            return $res;
-        }
 
-    }
-    public function  update($query)
-    {
-        $res = mysql_query($query);
-        if(!$res)
-        {
-            return "Can't update data ".mysql_error();
+    public function getData($query) {
+        $result = $this->con->query($query);
+        if (!$result) {
+            return "Can't get data " . $this->con->error;
+        } else {
+            return $result;
         }
-        else
-        {
+    }
+
+    public function update($query) {
+        if ($this->con->query($query)) {
             return "true";
+        } else {
+            return "Can't update data " . $this->con->error;
         }
     }
-    public  function  updateData($table,$conColumn,$conValue,$data)
-    {
-        $updates=array();
-        if (count($data) > 0) {
-            foreach ($data as $key => $value) {
 
-                $value = mysql_real_escape_string($value); // this is dedicated to @Jon
-                $value = "'$value'";
-                $updates[] = "$key = $value";
-            }
+    public function updateData($table, $conColumn, $conValue, $data) {
+        $updates = array();
+        foreach ($data as $key => $value) {
+            $value = $this->con->real_escape_string($value);
+            $value = "'" . $value . "'";
+            $updates[] = "$key = $value";
         }
         $implodeArray = implode(', ', $updates);
-        $query ="UPDATE ".$table." SET ".$implodeArray." WHERE ".$conColumn."='".$conValue."'";
-       //var_dump($query);
-        $res = mysql_query($query);
-        if(!$res)
-        {
-            return "Can't Update data ".mysql_error();
-        }
-        else
-        {
+        $query = "UPDATE $table SET $implodeArray WHERE $conColumn = '$conValue'";
+        if ($this->con->query($query)) {
             return "true";
+        } else {
+            return "Can't Update data " . $this->con->error;
         }
     }
 
-    public  function delete($query)
-    {
-        $res = mysql_query($query);
-       // var_dump($query);
-        if(!$res)
-        {
-            return "Can't delete data ".mysql_error();
-        }
-        else
-        {
+    public function delete($query) {
+        if ($this->con->query($query)) {
             return "true";
+        } else {
+            return "Can't delete data " . $this->con->error;
         }
     }
 
-    public  function  getAutoId($prefix)
-    {
-        $uId="";
-        $q = "select number from auto_id where prefix='".$prefix."';";
+    public function getAutoId($prefix) {
+        $uId = "";
+        $q = "SELECT number FROM auto_id WHERE prefix = '$prefix'";
         $result = $this->getData($q);
-        $userId=array();
-        while($row = mysql_fetch_assoc($result))
-        {
+        $userId = array();
+        while ($row = $result->fetch_assoc()) {
+            array_push($userId, $row['number']);
+        }
 
-            array_push($userId,$row['number']);
-
+        if (strlen($userId[0]) >= 1) {
+            $uId = $prefix . "00" . $userId[0];
+        } elseif (strlen($userId[0]) == 2) {
+            $uId = $prefix . "0" . $userId[0];
+        } else {
+            $uId = $prefix . $userId[0];
         }
-        // var_dump($UserId);
-        if(strlen($userId[0])>=1)
-        {
-            $uId=$prefix."00".$userId[0];
-        }
-        elseif(strlen($userId[0])==2)
-        {
-            $uId=$prefix."0".$userId[0];
-        }
-        else
-        {
-            $uId=$prefix.$userId[0];
-        }
-        array_push($userId,$uId);
+        array_push($userId, $uId);
         return $userId;
-
     }
-    public  function  updateAutoId($value,$prefix)
-    {
-         $id =intval($value)+1;
 
-        $query="UPDATE auto_id set number=".$id." where prefix='".$prefix."';";
+    public function updateAutoId($value, $prefix) {
+        $id = intval($value) + 1;
+        $query = "UPDATE auto_id SET number = $id WHERE prefix = '$prefix'";
         return $this->update($query);
-
     }
 
-    public  function execNonQuery($query)
-    {
-        $res = mysql_query($query);
-        if(!$res)
-        {
-            return "Can't Execute Query".mysql_error();
-        }
-        else
-        {
+    public function execNonQuery($query) {
+        if ($this->con->query($query)) {
             return "true";
-        }
-    }
-    public  function execDataTable($query)
-    {
-        $res = mysql_query($query);
-        if(!$res)
-        {
-            return "Can't Execute Query".mysql_error();
-        }
-        else
-        {
-            return $res;
+        } else {
+            return "Can't Execute Query" . $this->con->error;
         }
     }
 
+    public function execDataTable($query) {
+        $result = $this->con->query($query);
+        if (!$result) {
+            return "Can't Execute Query" . $this->con->error;
+        } else {
+            return $result;
+        }
+    }
 }
